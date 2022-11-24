42441a79df5f520884c2f3c9f499b775b036aaa3
dapplion <35266934+dapplion@users.noreply.github.com>
2022-11-24 08:31:14 +0100

    Add only -d:const_preset=gnosis

--- build/docker/dist/entry_point.sh
+++ build/docker/dist/entry_point.sh
@@ -6,7 +6,7 @@ cd /home/user/nimbus-eth2
 
 make \
     -j$(nproc) \
-    NIMFLAGS="-d:disableMarchNative,-d:const_preset=gnosis" \
+    NIMFLAGS="-d:const_preset=gnosis" \
     PARTIAL_STATIC_LINKING=1 \
     QUICK_AND_DIRTY_COMPILER=1 \
     gnosis-chain-build
